4a4cd5c8f9d66d80e93cfb25eb5b71724feb1355
OptimStack <sdutta.dev.engineer@gmail.com>
2026-09-04 16:35:20 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from "#src/App.jsx"
 import './index.css'
 
 // Mount the React application to the root DOM element
