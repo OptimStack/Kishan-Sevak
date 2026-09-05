3beebe31f3d4dee9d29c4ecbd4e6a83bcefbda09
OptimStack <sdutta.dev.engineer@gmail.com>
2026-09-05 10:02:58 +0530

    Update

--- src/views/index.js
+++ src/views/index.js
@@ -2,6 +2,6 @@ import FarmerDash from "#views/FarmerDash.jsx";
 import BuyerDash from "#views/BuyerDash.jsx";
 import AdminDash from "#views/AdminDash.jsx";
 import CropRegistration from "#views/CropRegistration.jsx";
-import listingsStore from "#views/ListingsStore.js";
+import { listingsStore } from "#views/ListingsStore.js";
 
 export { FarmerDash, BuyerDash, AdminDash, CropRegistration, listingsStore };
